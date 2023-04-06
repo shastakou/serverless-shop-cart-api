7d52948df9bcfbe421a00693ceee70cb2352f803
Ivan Shastakou <ivan_shastakou2@epam.com>
2023-04-06 18:10:33 +0200

    task-8: Updated project dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
-
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [
-    AuthModule,
-    CartModule,
-    OrderModule,
-  ],
-  controllers: [
-    AppController,
-  ],
+  imports: [ConfigModule.forRoot(), AuthModule, CartModule, OrderModule],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
