6cea5a1eacaf0c762d09c9ed7f1bb58bf220e6c9
Ivan Shastakou <ivan_shastakou2@epam.com>
2023-04-10 14:10:10 +0200

    task-8: Configured prisma for db access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { CartModule } from './cart/cart.module';
@@ -7,7 +8,23 @@ import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, CartModule, OrderModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          loggingMiddleware({
+            logger: new Logger('PrismaMiddleware'),
+            logLevel: 'log',
+          }),
+        ],
+      },
+    }),
+    AuthModule,
+    CartModule,
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { Callback, Context, Handler } from 'aws-lambda';
 import serverlessExpress from '@vendia/serverless-express';
+import { PrismaService } from 'nestjs-prisma';
 
 import { AppModule } from './app.module';
 
@@ -9,12 +10,12 @@ let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
 
-  app.enableCors({
-    origin: (req, callback) => callback(null, true),
-  });
+  app.enableCors();
   app.use(helmet());
 
+  await prismaService.enableShutdownHooks(app);
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
